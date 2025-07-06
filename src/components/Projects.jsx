d0a2806e587ca0c1fd93683c8cd539c8a93d6083
ara duco <araduco3@gmail.com>
2025-07-07 00:30:50 +0800

    Hardcode the the image base path

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ function Projects() {
   const projectList = [
     {
       title: 'Simple Inventory System',
-      image: 'project1.png', //C:\web-dev-projects\portfolio-site\public\project1.png
+      image: 'project1.png', //image file path:  C:\web-dev-projects\portfolio-site\public\project1.png
       tech: 'PHP, PostgreSQL, React',
       text: 'You can visit my project frontend with these credentials: \n\nUsername: admin \nPassword: admin123',
       link: 'https://github.com/ara-11/inventory-backend-api'
@@ -19,7 +19,7 @@ function Projects() {
       <div className="projects-grid">
         {projectList.map((project, index) => (
           <div className="project-card" key={index}>
-            <img src={`${process.env.PUBLIC_URL}/${project.image}`} alt={project.title} className="project-image" />
+            <img src={`/portfolio-site/${project.image}`} alt={project.title} />
             <h3 className="project-name">{project.title}</h3>
             <p className="project-tech">{project.tech}</p>
             <p className="project-text">{project.text}</p>
