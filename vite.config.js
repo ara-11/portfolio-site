52ded9a84a93b17b6c62ed72e6cac2f7a37df770
ara duco <araduco3@gmail.com>
2025-07-23 17:53:05 +0800

    change base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-site/', // ✅ Important for GitHub Pages
+  base: '/ara-mae-duco/', // ✅ Important for GitHub Pages
   plugins: [react()],
 })
